5256cbd905c9f3e0cf925e984a0dc0725b5b6aa9
ameyasawant99 <57026495+ameyasawant99@users.noreply.github.com>
2019-11-21 12:10:32 +0530

    TEE Submission

---
+++ assets/js/TEE.js
@@ -0,0 +1,34 @@
+// JavaScript Document
+$(document).ready(function() {
+"use strict";
+	
+	$( function() {
+    $( "input" ).checkboxradio();
+    $( "fieldset" ).controlgroup();
+  } );
+	
+	var spinner = $( "#spinner" ).spinner();
+ 
+    $( "#disable" ).on( "click", function() {
+      if ( spinner.spinner( "option", "disabled" ) ) {
+        spinner.spinner( "enable" );
+      } else {
+        spinner.spinner( "disable" );
+      }
+    });
+    $( "#destroy" ).on( "click", function() {
+      if ( spinner.spinner( "instance" ) ) {
+        spinner.spinner( "destroy" );
+      } else {
+        spinner.spinner();
+      }
+    });
+    $( "#getvalue" ).on( "click", function() {
+      alert( spinner.spinner( "value" ) );
+    });
+    $( "#setvalue" ).on( "click", function() {
+      spinner.spinner( "value", 5 );
+    });
+ 
+    $( "button" ).button();
+  } );
